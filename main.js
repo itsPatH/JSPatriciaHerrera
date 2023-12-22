ce56f048d50c2805c7bf8f803a23904167abf10e
Patricia <herrerapatriciadg@gmail.com>
2023-12-22 14:30:53 -0300

    Creacion de carrito de compras

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let nombre = prompt ("Por favor ingrese su nombre para ser atendido");{
+/*let nombre = prompt ("Por favor ingrese su nombre para ser atendido");{
         alert (`Hola, ${nombre}! Bienvenido, selecciona el producto al que quieras calcular el IVA en el siguiente paso`);
     }
 // Función para el cálculo del IVA
@@ -27,7 +27,7 @@ function calcularIVA(precioSinIVA, porcentajeIVA) {
   
       if (isNaN(seleccion) || seleccion < 1 || seleccion > productos.length) {
         alert("Selección no válida. Por favor, elija un número entre 1 y 3.");
-        continue;
+        continue; 
       }
   
       const productoSeleccionado = productos[seleccion - 1];
@@ -50,4 +50,71 @@ function calcularIVA(precioSinIVA, porcentajeIVA) {
   
   // call
   calcularIVAProductos();
+  */
+ 
+  const products = [
+    { name: "The American Burger", price: 6990 },
+    { name: "The Fixed Burger", price: 6990 },
+    { name: "The Code Burger", price: 7990 },
+    { name: "Blonde Beer", price: 1250 },
+    { name: "Red Beer", price: 1250 },
+    { name: "Dark Beer", price: 1250 },
+  ];
   
+  let shoppingCart = [];
+  
+  let selection = prompt(
+    "Bienvenido a The Code Bar, ¿Desea continuar? si/no"
+  );
+  while (selection !== "si" && selection !== "no") {
+    alert("Por favor ingresar Si o No");
+    selection = prompt("¿Desea continuar?");
+  }
+  
+  if (selection === "si") {
+    alert("Seleccione sus productos a continuación");
+    let showProducts = products.map(
+      (product) => product.name + " " + "$" + product.price
+    );
+    alert(showProducts.join(" / "));
+  } else if (selection === "no") {
+    alert("Gracias por visitarnos, esperamos verte de nuevo por acá");
+  }
+  
+  while (selection !== "no") {
+    let product = prompt("Realiza tu pedido");
+    let price = 0;
+    if (
+      product === "The American Burger" ||
+      product === "The Fixed Burger" ||
+      product === "The Code Burger" ||
+      product === "Blonde Beer" ||
+      product === "Red Beer" ||
+      product === "Dark Beer"
+    ) {
+      switch (product) {
+        case "The American Burger":
+        case "The Fixed Burger":
+          price = 6990;
+          break;
+        case "The Code Burger":
+          price = 7990;
+          break;
+        case "Blonde Beer":
+        case "Red Beer":
+        case "Dark Beer":
+          price = 1250;
+          break;
+        default:
+          break;
+      }
+      let quantity = parseInt(
+        prompt("Ingrese la cantidad que desea llevar de estos productos")
+      );
+  
+      shoppingCart.push({ product, quantity, price });
+      console.log(shoppingCart);
+    } else {
+      alert("Opción inválida");
+    }
+  }
